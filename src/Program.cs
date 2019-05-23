5935eecef1db0040881e2c3584f64a16592eb588
jskyzero <jskyzero@outlook.com>
2019-05-23 21:21:54 +0800

    :ok_hand: make this more like a project

---
+++ src/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace protobuf.csharp.example {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
