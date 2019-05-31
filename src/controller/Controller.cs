9705a835d137d48eb4fb06fbf9d96cbb2308f3b6
jskyzero <jskyzero@outlook.com>
2019-05-31 23:38:50 +0800

    :construction: add main loop

--- src/controller/Controller.cs
+++ src/controller/Controller.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Google.Protobuf;
+using Google.Protobuf.Collections;
 using Protobuf.Csharp.Example.Model;
 
 namespace Protobuf.Csharp.Example.Controller {
 
   public class Controller : IDisposable {
-    private static Users users = new Users();
-    private static Books books = new Books();
+    private static Users _users = new Users();
+    private static Books _books = new Books();
+    private RepeatedField<User> users => _users.Users_;
+    private RepeatedField<Book> books => _books.Books_;
     private static BorrowRecords records = new BorrowRecords();
     private static Dictionary<string, Tuple<Action<FileStream>, Action<FileStream>>> fileMapDict =
       new Dictionary<string, Tuple<Action<FileStream>, Action<FileStream>>> {
@@ -17,20 +20,20 @@ namespace Protobuf.Csharp.Example.Controller {
           "user.dat",
           new Tuple<Action<FileStream>, Action<FileStream>>(
             new Action<FileStream>((input) => {
-              users = Users.Parser.ParseFrom(input);
+              _users = Users.Parser.ParseFrom(input);
             }),
             new Action<FileStream>((output) => {
-              users.WriteTo(output);
+              _users.WriteTo(output);
             }))
         },
         {
           "books.dat",
           new Tuple<Action<FileStream>, Action<FileStream>>(
             new Action<FileStream>((input) => {
-              books = Books.Parser.ParseFrom(input);
+              _books = Books.Parser.ParseFrom(input);
             }),
             new Action<FileStream>((output) => {
-              books.WriteTo(output);
+              _books.WriteTo(output);
             }))
         },
         {
@@ -48,7 +51,7 @@ namespace Protobuf.Csharp.Example.Controller {
     private static string kConfigFolderName = "./.config/";
     private static string kAdministratorAccountName = "Administrator";
 
-    public bool ContainNoAdministratorAccount => users.Users_.Where(
+    public bool ContainNoAdministratorAccount => _users.Users_.Where(
       user => user.Name == kAdministratorAccountName).Count() == 0;
 
     public Controller() {
@@ -66,17 +69,45 @@ namespace Protobuf.Csharp.Example.Controller {
       LogHelper.Log(LogType.DEBUG, "SaveDataFile2 End");
     }
 
+    public bool isUserNameExist(string name) {
+      return users.Where(user => user.Name == name).Count() != 0;
+    }
+
     public void AddUser(string name, string password) {
       var user = new User {
         Name = name,
         Password = password,
       };
-      users.Users_.Add(user);
+      users.Add(user);
       LogHelper.Log(LogType.DEBUG, user.ToString());
     }
 
+    public bool BorrowBook(string name) {
+      return false;
+    }
+
+    public bool ReturnBook(string name) {
+      return false;
+    }
+
+    public string FindBooks(string name) {
+      var list = books.Where(book => book.Name == name).Select(book => {
+        return String.Format("Name: {0}\ndetails: {1}\namount: {2}\n", 
+        book.Name, book.Details, book.Amount);
+      });
+      return String.Join("\n", list);
+    }
+
     public bool CheckUserPassword(string name, string password) {
-      return users.Users_.Where(user => user.Name ==name && user.Password == password).Count() == 1;
+      return users.Where(user => user.Name == name && user.Password == password).Count() == 1;
+    }
+
+    public bool UpdateUserPassword(string name, string password, string newPassword) {
+      return users.Where(user => user.Name == name && user.Password == password).Count() == 1;
+    }
+
+    public bool CheckIsAdministratorUser(string name, string password) {
+      return users.Where(user => user.Name == name && user.Password == password && user.Roletype == User.Types.RoleType.Administrator).Count() == 1;
     }
 
     public void AddAdministratorUser(string password) {
--- src/viewer/Viewer.cs
+++ src/viewer/Viewer.cs
@@ -5,12 +5,13 @@ using System.Text;
 using Protobuf.Csharp.Example.Controller;
 
 namespace Protobuf.Csharp.Example.Viewer {
-  internal class Viewer : IDisposable{
+  internal class Viewer : IDisposable {
     private static string kSaltString = "jskyzero";
     private string name = "";
     private string password = "";
     private bool isPasswordRight = false;
-
+    private bool isAdministratorAccount = false;
+    private bool isLogout => isPasswordRight;
 
     private Controller.Controller controller = new Controller.Controller();
 
@@ -26,12 +27,126 @@ namespace Protobuf.Csharp.Example.Viewer {
       while (!isPasswordRight)
         UserLogin();
       // main function
-      while (isPasswordRight) {
-
+      while (isLogout) {
+        PrintMenu();
+        PrintInputCharacter();
+        MainLoop();
       }
     }
 
-    private void PrintCharacter() {
+    private void ExitSystem() {
+      Console.Out.WriteLine("Exit System");
+      password = string.Empty;
+      isPasswordRight = false;
+    }
+
+    private void CheckBooks() {
+      Console.Out.WriteLine("Enter book name");
+      PrintInputCharacter();
+      string bookname = Console.In.ReadLine();
+      string findResult = controller.FindBooks(bookname);
+      Console.WriteLine(findResult == string.Empty ? "No Such Name Book" : findResult);
+    }
+
+    private void BorrowBook() {
+      Console.Out.WriteLine("Enter book name");
+          PrintInputCharacter();
+          string bookname = Console.In.ReadLine();
+          string findResult = controller.FindBooks(bookname);
+          Console.WriteLine(findResult == string.Empty ? "No Such Name Book" : findResult);
+    }
+
+    private void ReturnBook() {
+      Console.Out.WriteLine("Enter book name");
+          PrintInputCharacter();
+          string bookname = Console.In.ReadLine();
+          string findResult = controller.FindBooks(bookname);
+          Console.WriteLine(findResult == string.Empty ? "No Such Name Book" : findResult);
+    }
+
+    private void ListBorrowRecord() {
+      Console.Out.WriteLine("Enter book name");
+          PrintInputCharacter();
+          string bookname = Console.In.ReadLine();
+          string findResult = controller.FindBooks(bookname);
+          Console.WriteLine(findResult == string.Empty ? "No Such Name Book" : findResult);
+    }
+
+    private void ChangePassword() {
+      Console.Out.WriteLine("Enter book name");
+          PrintInputCharacter();
+          string bookname = Console.In.ReadLine();
+          string findResult = controller.FindBooks(bookname);
+          Console.WriteLine(findResult == string.Empty ? "No Such Name Book" : findResult);
+    }
+
+    private void AddUser() {
+      Console.Out.WriteLine("Enter book name");
+          PrintInputCharacter();
+          string bookname = Console.In.ReadLine();
+          string findResult = controller.FindBooks(bookname);
+          Console.WriteLine(findResult == string.Empty ? "No Such Name Book" : findResult);
+    }
+
+    private void AddBooks() {
+      Console.Out.WriteLine("Enter book name");
+          PrintInputCharacter();
+          string bookname = Console.In.ReadLine();
+          string findResult = controller.FindBooks(bookname);
+          Console.WriteLine(findResult == string.Empty ? "No Such Name Book" : findResult);
+    }
+
+    private void ShowAllRecord() {
+      Console.Out.WriteLine("Enter book name");
+          PrintInputCharacter();
+          string bookname = Console.In.ReadLine();
+          string findResult = controller.FindBooks(bookname);
+          Console.WriteLine(findResult == string.Empty ? "No Such Name Book" : findResult);
+    }
+
+    private void DeleteUser() {
+      Console.Out.WriteLine("Enter book name");
+          PrintInputCharacter();
+          string bookname = Console.In.ReadLine();
+          string findResult = controller.FindBooks(bookname);
+          Console.WriteLine(findResult == string.Empty ? "No Such Name Book" : findResult);
+    }
+
+    private void MainLoop() {
+      PrintInputCharacter();
+      string userChoice = Console.In.ReadLine();
+      switch (userChoice) {
+        case "0":
+          ExitSystem();
+          break;
+        case "1": // 1: Check Books
+          CheckBooks();
+          break;
+        case "2": // 2: Borrow Books
+          BorrowBook();
+          break;
+        case "3": // 3: Return Books
+          break;
+        case "4": // 4: List Borrow Record
+          break;
+        case "5": // 5: Change Password");
+          break;
+        case "6": // 6: (Administrator only) Add User
+          break;
+        case "7": // 7: (Administrator only) Add Books
+          break;
+        case "8": // 8: (Administrator only) Show All Record
+          break;
+        case "9": // 9: (Administrator only) Delete User
+          break;
+        default:
+          Console.Out.WriteLine("Your input is {0}", userChoice);
+          Console.Out.WriteLine("Can't find match methods, please check your input");
+          break;
+      }
+    }
+
+    private void PrintInputCharacter() {
       Console.Out.Write("$> ");
     }
 
@@ -41,18 +156,21 @@ namespace Protobuf.Csharp.Example.Viewer {
       2: Borrow Books
       3: Return Books
       4: List Borrow Record
-      5: Change Password
+      5: Change Password");
+      if (isAdministratorAccount)
+        Console.Out.WriteLine(@"
       6: (Administrator only) Add User
       7: (Administrator only) Add Books
       8: (Administrator only) Show All Record
-      9: (Administrator only) Delete User
-
+      9: (Administrator only) Delete User");
+      Console.Out.WriteLine(@"
+      0: Exit System
       Enter your choices(use number)");
     }
 
     private void InitialAdministratorAccount() {
       Console.Out.WriteLine("First time running, please set password for default administrator account");
-      PrintCharacter();
+      PrintInputCharacter();
       password = PasswordHash(Console.In.ReadLine(), kSaltString);
       controller.AddAdministratorUser(password);
     }
@@ -60,19 +178,21 @@ namespace Protobuf.Csharp.Example.Viewer {
     private void UserLogin() {
       Console.Out.WriteLine("[User Login]");
       Console.Out.WriteLine("Input your user name");
-      PrintCharacter();
+      PrintInputCharacter();
       name = Console.In.ReadLine();
       Console.Out.WriteLine("Input your password");
-      PrintCharacter();
+      PrintInputCharacter();
       password = PasswordHash(Console.In.ReadLine(), kSaltString);
       isPasswordRight = controller.CheckUserPassword(name, password);
+      isAdministratorAccount = controller.CheckIsAdministratorUser(
+        name, password);
     }
 
     private static string PasswordHash(string password, string salt) {
       byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
       byte[] saltBytes = Encoding.UTF8.GetBytes(salt);
       byte[] saltedValue = passwordBytes.Concat(saltBytes).ToArray();
-      byte[] hashedValue  = new SHA256Managed().ComputeHash(saltedValue);
+      byte[] hashedValue = new SHA256Managed().ComputeHash(saltedValue);
       return Encoding.ASCII.GetString(hashedValue);
     }
   }
